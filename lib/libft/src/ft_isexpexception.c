cf6c566bd5bc5e7a74a18aa14016a2142b156b37
Boris Gazur <barlukh@gmail.com>
2025-08-28 14:15:53 +0300

    Fix $ exp for tester

--- lib/libft/src/ft_isexpexception.c
+++ lib/libft/src/ft_isexpexception.c
@@ -6,7 +6,7 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 14:12:12 by bgazur            #+#    #+#             */
-/*   Updated: 2025/08/28 10:47:48 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/08/28 14:14:24 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 bool	ft_isexpexception(char *content, char quote, size_t i)
 {
-	if (content[i + 1] == '\0' || (ft_isifs(content[i + 1]) && quote != '\0')
-		|| (ft_isquote(content[i + 1]) && quote != '\0'))
+	char	next;
+
+	next = content[i + 1];
+	if (next == '\0')
 		return (true);
+	if (quote != '\0')
+	{
+		if (next == '?' || next == '_' || ft_isalpha(next))
+			return (false);
+		else
+			return (true);
+	}
 	return (false);
 }
